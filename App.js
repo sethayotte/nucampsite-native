c750b82dceea504e75a7829830bade57040737c9
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-10-14 21:07:52 -0400

    App Nav

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './components/MainComponent';
+
 
 export default function App() {
     return (
