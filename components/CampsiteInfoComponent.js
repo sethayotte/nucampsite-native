4a6534bbf2a1cf0ddc79c8ff2e35b1f71ad8de7b
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-11-06 18:20:23 -0500

    Social Sharing, Image/Camera API, Network Toast

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Text, View, ScrollView, FlatList,
   Modal, Button, StyleSheet,
-  Alert, PanResponder } from 'react-native';
+  Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import * as Animatable from 'react-native-animatable';
 import { connect } from "react-redux";
@@ -65,6 +65,16 @@ function RenderCampsite(props) {
     }
 });
 
+const shareCampsite = (title, message, url) => {
+  Share.share({
+      title: title,
+      message: `${title}: ${message} ${url}`,
+      url: url
+  },{
+      dialogTitle: 'Share ' + title
+  });
+};
+
   if (campsite) {
       return (
               <Animatable.View
@@ -98,6 +108,14 @@ function RenderCampsite(props) {
             raised
             reverse
             onPress={() => props.onShowModal()}
+          />
+          <Icon
+            name={'share'}
+            type='font-awesome'
+            color='#5637DD'
+            raised
+            reverse
+            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
           />
         </View>
         </Card>
